a05cf2047d1707ee5a1971d5dd75beb4c06f8720
AtulBangre <2604atulbangre@gmail.com>
2023-09-12 17:01:36 +0530

    anydesk clone

---
+++ Anydesk clone/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      'sm': '600px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '991px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1400px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
